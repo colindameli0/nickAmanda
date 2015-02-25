4fcd17486bd602b1a81156726d5806e90230800d
colindameli0 <colin.damelio@gmail.com>
2015-02-24 21:20:10 -0500

    ooooo baaaby

---
+++ dist/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	var map;
+	function initialize() {
+	  var mapOptions = {
+	    zoom: 13,
+	    center: new google.maps.LatLng(-34.397, 150.644)
+	  };
+	  map = new google.maps.Map(document.getElementById('map-canvas'),
+	      mapOptions);
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+});
