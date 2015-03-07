557768e5a7f4c966cf85d171bbe8b43903f0d7c7
colindameli0 <colin.damelio@gmail.com>
2015-03-06 21:55:52 -0500

    updates biatch

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,45 @@
 $(document).ready(function(){
 
-	var map;
+
+	//GOOGLE MAPS API
+
 	function initialize() {
+	  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
 	  var mapOptions = {
-	    zoom: 13,
-	    center: new google.maps.LatLng(-34.397, 150.644)
-	  };
-	  map = new google.maps.Map(document.getElementById('map-canvas'),
-	      mapOptions);
+	    zoom: 14,
+	    scrollwheel: false,
+	    center: myLatlng
+	  }
+	  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	  var marker = new google.maps.Marker({
+	      position: myLatlng,
+	      map: map,
+	      title: 'Hello World!'
+	  });
 	}
 
 	google.maps.event.addDomListener(window, 'load', initialize);
+
+
+	//STICKY NAV
+
+	  $("nav").sticky({ 
+	  	topSpacing: 0 
+	  });
+
+	  //SMOOTH SCROLL MENU TOGGLE
+	  
+	  $('a[href^="#"]').click(function(e) {
+	      var target = $(this.hash);
+	      if (target.length === 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+	      if (target.length === 0) target = $('html');
+	      $('html, body').animate({
+	          scrollTop: target.offset().top -85
+	      }, 500);
+	      return false;
+	  });
+
+	  $('.single-item').slick();
 
 });
